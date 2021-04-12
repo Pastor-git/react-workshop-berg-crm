7de315a5057526618472e2dc972798e6e998b0ae
Pastor-git <maciej.pastor@gmail.com>
2021-04-13 00:41:31 +0200

    Redirect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, BrowserRouter as Router, NavLink } from "react-router-dom";
+import { Home } from "./components/Home";
+import { BergForm } from "./components/BergForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+          <li><NavLink exact to="/home">Home</NavLink></li>
+          <li><NavLink to="/formular">BergForm</NavLink></li>
+        </ul>
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/formular">
+            <BergForm />
+          </Route>
+          <Route path="*">
+            <div>404</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+// import { useParams } from "react-router-dom";
+import  Button  from "@material-ui/core/Button";
+
+export const Home = () => {
+    // const { id } = useParams();
+    const navigateHome = () => {
+      console.log("navigate home");
+    };
+    const navigateBack = () => {
+      console.log("navigate back");
+    };
+    const windowOpener = () => {
+        window.open('https://app.bergsystem.pl/conference_confirm/?id=17&system=appdb1766&user=4b591c30fc20ac2f1a2d1b5c704edb01&view=outside');
+    }
+
+    return (
+      <div>
+        <h2>Karols's App</h2>
+        <Button variant="outlined" onClick={navigateHome}>
+          Navigate home
+        </Button>
+        <Button variant="outlined" onClick={navigateBack}>
+          Go back
+        </Button>
+        <Button variant="outlined" onClick={windowOpener}>
+            Berg Form
+        </Button>
+      </div>
+    );
+}
