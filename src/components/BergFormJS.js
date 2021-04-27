e0cdb13b11e1e663445b092c0ac0b55fabcb167f
Pastor-git <maciej.pastor@gmail.com>
2021-04-27 12:36:18 +0200

    Test na gmail

--- src/components/BergFormJS.js
+++ src/components/BergFormJS.js
@@ -4,6 +4,6 @@ import React from "react";
 
 export default function BergFormJS() {
 return (
- <div data-height="330"  class="conferenceWebForms" data-berg-webforms="https://app.bergsystem.pl/conference_confirm/?id=17&system=appdb1766&user=4b591c30fc20ac2f1a2d1b5c704edb01&view=outside"><script src="https://app.bergsystem.pl/js/conference/conference.min.js"></script></div>       
+    <div data-height="890" class="conferenceWebForms" data-berg-webforms="https://app.bergsystem.pl/conference_confirm/?id=18&system=appdb1766&user=9362617868f15f796a1de4b8205345ae&view=outside"><script src="https://app.bergsystem.pl/js/conference/conference.min.js"></script></div>
     );
 }
